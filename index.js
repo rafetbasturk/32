be3179adf39d0f4f6759743c1408cc1ba1d30d81
Rafet <rafet@Rafet-MacBook-Air.local>
2021-03-12 15:19:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const fetch = require('node-fetch')
+
+const app = express()
+app.set('view engine', 'ejs')
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+const contents = []
+
+app.get("/", (req, res) => {
+    res.render("index", {contents});
+})
+
+app.post('/', (req, res) => {
+    const url = "https://api.shrtco.de/v2/shorten?url=";
+    const inputUrl = req.body.shorten;
+    const postUrl = url + inputUrl;
+
+    (async () => {
+        try {
+            const response = await fetch(postUrl)
+            const data = await response.json()
+            console.log(data.ok)
+            let result = ""
+            if (inputUrl.indexOf("https") === 0) {
+                result = {startingUrl: data.result.original_link, shortened: "https://rafet/" + data.result.code}
+            } else {
+                result = {startingUrl: data.result.original_link, shortened: "http://rafet/" + data.result.code}
+            }
+            contents.push(result);
+        } finally {
+            res.render("index", {contents})
+        }
+    })()
+})
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
---
+++ public/script.js
@@ -0,0 +1,55 @@
+// elements
+const hamburger = document.querySelector("#hamburger")
+const closeIcon = document.querySelector("#close")
+const nav = document.querySelector(".header__nav")
+const btn = document.querySelector(".main__form__button")
+const inp = document.querySelector(".main__form__div--input")
+const i = document.querySelector("i")
+
+// event listeners
+hamburger.addEventListener("click", () => {
+    closeIcon.style.display = "block"
+    hamburger.style.display = "none"
+    nav.style.display = "block"
+})
+
+closeIcon.addEventListener("click", () => {
+    hamburger.style.display = "block"
+    closeIcon.style.display = "none"
+    nav.style.display = "none"
+})
+
+btn.addEventListener("click", (e) => {
+    if (inp.value == "") {
+        e.preventDefault()
+        i.style.display = "block"
+        inp.style.borderColor = "hsl(0, 87%, 67%)"
+        setTimeout(() => {
+            i.style.display = "none"
+            inp.style.borderColor = "transparent"
+        }, 5000)
+    }
+})
+
+inp.addEventListener("click", () => {
+    i.style.display = "none"
+    inp.style.borderColor = "transparent"
+})
+
+document.querySelector(".main").addEventListener("click", (e) => {
+    if (e.target.classList.contains("copy")) {
+        e.preventDefault()
+        let r = document.createRange()
+        r.selectNode(e.target.parentNode.previousElementSibling.children[1])
+        window.getSelection().removeAllRanges()
+        window.getSelection().addRange(r)
+        document.execCommand('copy')
+        window.getSelection().removeAllRanges()
+        e.target.style.background = "hsl(257, 27%, 26%)"
+        e.target.innerText = "Copied!"
+        setTimeout(() => {
+            e.target.style.background = "hsl(180, 66%, 49%)"
+            e.target.innerText = "Copy"
+        }, 5000)
+    }
+})
